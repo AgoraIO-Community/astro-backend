55d24e3ebfb9831929a3279d95c7b636a9749a8e
Tadas Petra <tadas@tadaspetra.com>
2024-06-19 12:42:22 -0500

    updated logic

--- src/pages/rtc/[channel]/[role]/[uid].json.ts
+++ src/pages/rtc/[channel]/[role]/[uid].json.ts
@@ -4,12 +4,11 @@ import type { APIContext } from "astro";
 const APP_ID = import.meta.env.APP_ID;
 const APP_CERTIFICATE = import.meta.env.APP_CERTIFICATE;
 
-export async function GET({ params }: APIContext): Promise<Response> {
+export async function GET({ params }: APIContext) {
     // Set CORS headers
     const headers = new Headers({
         "Access-Control-Allow-Origin": "*",
         "Access-Control-Allow-Methods": "GET, OPTIONS",
-        "Access-Control-Allow-Headers": "Content-Type"
     });
 
     //check for valid channel
@@ -20,7 +19,7 @@ export async function GET({ params }: APIContext): Promise<Response> {
     let role;
     if (params.role === 'publisher') {
         role = agoraToken.RtcRole.PUBLISHER;
-    } else if (params.role === 'audience') {
+    } else if (params.role === 'subscriber') {
         role = agoraToken.RtcRole.SUBSCRIBER
     } else {
         return new Response("role is incorrect", { status: 400, headers })
@@ -33,13 +32,9 @@ export async function GET({ params }: APIContext): Promise<Response> {
     const expireTime = 600;
     const privilegeExpireTime = 600;
     let token;
-    if (params.tokentype === 'userAccount') {
-        token = agoraToken.RtcTokenBuilder.buildTokenWithUserAccount(APP_ID, APP_CERTIFICATE, params.channel, params.uid, role, expireTime, privilegeExpireTime);
-    } else if (params.tokentype === 'uid') {
-        token = agoraToken.RtcTokenBuilder.buildTokenWithUid(APP_ID, APP_CERTIFICATE, params.channel, params.uid, role, expireTime, privilegeExpireTime);
-    } else {
-        return new Response("token type is invalid", { status: 400, headers })
-    }
+
+    token = agoraToken.RtcTokenBuilder.buildTokenWithUid(APP_ID, APP_CERTIFICATE, params.channel, params.uid, role, expireTime, privilegeExpireTime);
+
     return new Response(JSON.stringify({
         rtcToken: token
     }), { headers })
