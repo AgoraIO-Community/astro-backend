0e3e1cdbe5123123f4ff3faefaa488d32602d987
Tadas Petra <tadas@tadaspetra.com>
2024-09-12 13:44:25 -0500

    Update with working transcription UI

--- src/components/Call.tsx
+++ src/components/Call.tsx
@@ -10,7 +10,23 @@ import AgoraRTC, {
   useRTCClient,
   useRemoteUsers,
 } from "agora-rtc-react";
-import { useCallback, useEffect, useState } from "react";
+import type { Dispatch, SetStateAction } from "react";
+import { useCallback, useEffect, useMemo, useState } from "react";
+
+import { astrobackend } from "../proto/definition";
+
+function Transcriptions({ transcriptions }: { transcriptions: string[] }) {
+  return (
+    <div className="fixed z-10 top-0 right-0 w-1/4 h-full bg-white bg-opacity-75 overflow-y-auto p-4">
+      <h2 className="text-lg font-bold mb-2">Transcriptions</h2>
+      {transcriptions.map((text, index) => (
+        <p key={index} className="mb-2">
+          {text}
+        </p>
+      ))}
+    </div>
+  );
+}
 
 type RecordingInfo = {
   uid: string;
@@ -19,20 +35,30 @@ type RecordingInfo = {
   resourceId: string;
 };
 
+type TranscriptionInfo = {
+  taskId: string;
+  builderToken: string;
+};
+
 function Call(props: {
   appId: string;
   channelName: string;
   token: string;
-  uid: string;
+  uid: number;
 }) {
-  const client = useRTCClient(
-    AgoraRTC.createClient({ codec: "vp8", mode: "rtc" })
+  const client = useMemo(
+    () => AgoraRTC.createClient({ codec: "vp8", mode: "rtc" }),
+    []
   );
 
   const [isRecording, setIsRecording] = useState(false);
   const [recordingInfo, setRecordingInfo] = useState<RecordingInfo | null>(
     null
   );
+  const [isTranscribing, setIsTranscribing] = useState(false);
+  const [transcriptionInfo, setTranscriptionInfo] =
+    useState<TranscriptionInfo | null>(null);
+  const [transcriptions, setTranscriptions] = useState<string[]>([]);
 
   useEffect(() => {
     let timer: NodeJS.Timeout | null = null;
@@ -46,6 +72,19 @@ function Call(props: {
       }
     };
   }, [isRecording]);
+
+  useEffect(() => {
+    let timer: NodeJS.Timeout | null = null;
+    if (isTranscribing) {
+      timer = setInterval(checkTranscriptionStatus, 10000);
+    }
+    return () => {
+      if (timer) {
+        clearInterval(timer);
+        timer = null;
+      }
+    };
+  }, [isTranscribing]);
 
   const checkRecordingStatus = useCallback(async () => {
     try {
@@ -71,6 +110,75 @@ function Call(props: {
     }
   }, [recordingInfo, setIsRecording]);
 
+  const checkTranscriptionStatus = useCallback(async () => {
+    try {
+      const response = await fetch("/api/transcription/query.json", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          taskId: transcriptionInfo!.taskId,
+          builderToken: transcriptionInfo!.builderToken,
+        }),
+      });
+
+      if (!response.ok) {
+        setIsTranscribing(false);
+        setTranscriptionInfo(null);
+      } else {
+        console.log("Transcription found and running");
+      }
+    } catch (error) {
+      console.error("Error checking transcription status:", error);
+    }
+  }, [transcriptionInfo, setIsTranscribing]);
+
+  const toggleTranscription = async () => {
+    if (isTranscribing) {
+      try {
+        const response = await fetch("/api/transcription/stop.json", {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify(transcriptionInfo),
+        });
+        if (response.ok) {
+          setIsTranscribing(false);
+          setTranscriptionInfo(null);
+          console.log("Transcription stopped");
+        } else {
+          console.error("Failed to stop transcription");
+        }
+      } catch (error) {
+        console.error("Error stopping transcription:", error);
+      }
+    } else {
+      try {
+        const response = await fetch("/api/transcription/start.json", {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify({
+            channel: props.channelName,
+          }),
+        });
+        if (response.ok) {
+          const data = await response.json();
+          setTranscriptionInfo(data);
+          setIsTranscribing(true);
+          console.log("Transcription started");
+        } else {
+          console.error("Failed to start transcription");
+        }
+      } catch (error) {
+        console.error("Error starting transcription:", error);
+      }
+    }
+  };
+
   return (
     <AgoraRTCProvider client={client}>
       <Videos
@@ -78,7 +186,9 @@ function Call(props: {
         AppID={props.appId}
         token={props.token}
         uid={props.uid}
+        setTranscriptions={setTranscriptions}
       />
+      <Transcriptions transcriptions={transcriptions} />
       <div className="fixed z-10 bottom-0 left-0 right-0 flex justify-center pb-4 space-x-4">
         <a
           className="px-5 py-3 text-base font-medium text-center text-white bg-red-400 rounded-lg hover:bg-red-500 w-40"
@@ -157,6 +267,21 @@ function Call(props: {
             Start Recording
           </div>
         )}
+        {isTranscribing ? (
+          <div
+            className="px-5 py-3 text-base font-medium text-center text-white bg-red-400 rounded-lg hover:bg-red-500 w-40"
+            onClick={toggleTranscription}
+          >
+            Stop Transcription
+          </div>
+        ) : (
+          <div
+            className="px-5 py-3 text-base font-medium text-center text-white bg-green-400 rounded-lg hover:bg-green-500 w-40"
+            onClick={toggleTranscription}
+          >
+            Start Transcription
+          </div>
+        )}
       </div>
     </AgoraRTCProvider>
   );
@@ -166,14 +291,17 @@ function Videos(props: {
   channelName: string;
   AppID: string;
   token: string;
-  uid: string;
+  uid: number;
+  setTranscriptions: Dispatch<SetStateAction<string[]>>;
 }) {
-  const { AppID, channelName, token, uid } = props;
+  const { AppID, channelName, token, uid, setTranscriptions } = props;
+
+  const client = useRTCClient();
+
   const { isLoading: isLoadingMic, localMicrophoneTrack } =
     useLocalMicrophoneTrack();
   const { isLoading: isLoadingCam, localCameraTrack } = useLocalCameraTrack();
   const remoteUsers = useRemoteUsers();
-  const client = useRTCClient();
 
   usePublish([localMicrophoneTrack, localCameraTrack]);
   useJoin({
@@ -181,6 +309,28 @@ function Videos(props: {
     channel: channelName,
     token: token,
     uid: uid,
+  });
+
+  useClientEvent(client, "stream-message", (_, msg) => {
+    try {
+      const decodedMsg = astrobackend.Text.deserializeBinary(msg);
+
+      const { words } = decodedMsg;
+
+      if (words && words.length > 0) {
+        let isFinal = false;
+        let text = "";
+        words.forEach((item: any) => {
+          if (item.is_final) {
+            setTranscriptions((prev) => [...prev, `${uid}: ${text}`]);
+            isFinal = true;
+          }
+          text += item?.text || "";
+        });
+      }
+    } catch (error) {
+      console.error("Error decoding stream message:", error);
+    }
   });
 
   useClientEvent(client, "token-privilege-will-expire", async () => {
